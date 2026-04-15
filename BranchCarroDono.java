0b333f7fcfb337fcfc8825f6df835e85c4bb2b43
Eduarda Barros - Pokkedex <eduardab.py@gmail.com>
2026-04-14 21:34:21 -0300

    BranchCarroDono 2.0

--- BranchCarroDono.java
+++ BranchCarroDono.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
 public class BranchCarroDono {
+    Scanner scanstr = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
 
     // ATRIBUTOS
     String nome;
-    int cpf;
+    String cpf;
 
     //MÉTODO REGISTRAR
-
+    void registrar(){
+        System.out.println("Digite o nome do dono: ");
+        nome = scanstr.nextLine();
+        System.out.println("Digite o cpf do dono: ");
+        cpf = scan.nextLine();
+    }
 
     // MÉTODO CONSULTAR
-
+    void consultar(){
+        System.out.println("NOME DO DONO: "+ nome);
+        System.out.println("CPF DO DONO: "+ cpf);
+    }
     
     // MÉTODO ATUALIZAR
+    void atualizar(){
+        registrar();
+    }
 }
